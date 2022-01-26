f2510c62aead08b4f7a2db01efc32914164a8778
Mohab Gabber <mohabgabber1@protonmail.com>
2022-01-26 12:15:23 +0200

    edited frontend in register

--- users/views.py
+++ users/views.py
@@ -2,13 +2,10 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import UserRegister
 from django.urls import reverse_lazy
-from django.contrib.auth.views import PasswordChangeView, PasswordResetDoneView
+from django.contrib.auth.views import PasswordChangeView
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.core.mail import send_mail, BadHeaderError
-from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.contrib.auth import authenticate, login, logout
-from django.views.generic.edit import CreateView
 def register(request):
     if request.method == 'POST':
         form = UserRegister(request.POST)
@@ -16,7 +13,11 @@ def register(request):
             form.username = request.POST.get('username')
             form.password1 = request.POST.get('password1')
             form.password2 = request.POST.get('password2')
-            form.save()
+            try:
+                form.save()
+            except:
+                messages.success(request, f'there is an error')
+                return render(request, 'users/register.html', {"form": form})
             username = form.cleaned_data.get('username')
             messages.success(request, f'account created for {username}')
             new_user = authenticate(username=form.cleaned_data['username'],password=form.cleaned_data['password1'],)
@@ -39,6 +40,8 @@ def LoginView(request):
 class PasswordChange(LoginRequiredMixin, PasswordChangeView):
     template_name = 'users/password_change.html'
     success_url = reverse_lazy('home')
+@login_required
 def logout_view(request):
     logout(request)
+    messages.success(request, 'logged out successfully')
     return redirect('login')
