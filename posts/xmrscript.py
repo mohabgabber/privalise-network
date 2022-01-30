e0e6d8fe89ee21a2412f7231a8333167680ac1cc
Mohab Gabber <mohabgabber1@protonmail.com>
2022-01-30 13:28:18 +0200

    fixed errors in xmrscript

--- posts/xmrscript.py
+++ posts/xmrscript.py
@@ -8,32 +8,35 @@ def create_addr(id):
     new_addr = w.new_address(label=id)
     return new_addr
 def pay(addr, amnt):
-    addr = address(addr)
-    if addr:
+    monero = address(addr)
+    if monero:
         tx = w.transfer(addr, Decimal(str(amnt)))
         return tx
     else:
         return False
-def receive(amnt, addr, hash):
+def valid_addr(addr):
+    try:
+        monero = address(addr)
+        return True
+    except:
+        return False
+def receive(amnt, addr, hashes):
     incoming = w.incoming(local_address=addr, unconfirmed=True, confirmed=True)
-    if incoming and incoming.amount == amnt and incoming.transaction.hash == hash and incoming.local_address == addr:
+    if incoming[0] and float(incoming[0].amount) >= float(amnt) and incoming[0].transaction.hash == hashes and incoming[0].local_address == addr:
         return True
     else:
         return False
 def check_addr(addr):
-    incoming = w.incoming(local_address=addr)
+    incoming = w.incoming(local_address=addr, unconfirmed=True, confirmed=True)
     if incoming[0]:
         return False
     else:
         return True
 def check_conf(addr, hash, amnt):
-    tx = w.incoming(local_address=addr)
-    if tx.amount == amnt and tx.transaction.hash == hash and tx.local_address == addr:
-        confs = w.confirmations(tx)
-        if confs >= 5:
-            return True
-        else:
-            return False
+    tx = w.incoming(local_address=addr, unconfirmed=True, confirmed=True)
+    confs = w.confirmations(tx[0])
+    if confs >= 5:
+        return True
     else:
         return False
 
