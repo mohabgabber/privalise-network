d79fe2132de3b683d914a73b8e3c1405d5aa712a
Mohab Gabber <mohabgabber1@protonmail.com>
2022-02-07 11:03:40 +0200

    added encrypted notes (not E2EE yet), added more view

--- posts/admin.py
+++ posts/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Post, Profile, Comment, Tag, Notification, About
+from .models import Post, Profile, Comment, Tag, Notification, Notes
 admin.site.register(Post)
 admin.site.register(Profile)
 admin.site.register(Comment)
 admin.site.register(Notification)
 admin.site.register(Tag)
-admin.site.register(About)
+admin.site.register(Notes)
